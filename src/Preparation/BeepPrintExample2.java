3c476614ebc9d793ad913647b23e902e8bb380c9
sumanChoi <voltovo87@gamil.com>
2020-02-05 05:35:38 +0900

    feat
    1. beep / printing  (동시진행)

---
+++ src/Preparation/BeepPrintExample2.java
@@ -0,0 +1,22 @@
+package Preparation;
+
+public class BeepPrintExample2 {
+
+	public static void main(String[] args) {
+		Runnable beepTask = new BeepTask();
+		Thread thread = new Thread(beepTask);
+		//BeepTask 객체의 run() 메소드가 실행
+		thread.start();
+		
+		for(int i = 0; i < 5; i++) {
+			System.out.println("띵");
+			try {
+				Thread.sleep(500);
+			} catch (Exception e) {
+				
+			}
+		}
+
+	}
+
+}
