80e3ff4cf587ddeb89c7e86b8226672df3982e9f
sumanChoi <voltovo87@gamil.com>
2020-02-06 21:27:43 +0900

    refactor
    동기화 calculator 주석 추가

--- src/Synchronized/Calculator.java
+++ src/Synchronized/Calculator.java
@@ -7,7 +7,9 @@ public class Calculator {
 		return this.memory;
 	}
 	
+	//계산기 메모리 값을 저장하는 메소드
 	public void setMemory(int memory) {
+		//매개 값을 memory에 저장
 		this.memory = memory;
 		try {
 			Thread.sleep(2000);
