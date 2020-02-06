8212399db5c611c07a829a8a5da6a9bca70db885
sumanChoi <voltovo87@gamil.com>
2020-02-06 20:53:54 +0900

    feat
    Thread 우선순위

---
+++ src/Preparation/PriorityExample.java
@@ -0,0 +1,19 @@
+package Preparation;
+
+public class PriorityExample {
+
+	public static void main(String[] args) {
+		for(int i = 1; i< 11; i++) {
+			Thread thread = new CalcThread("thread" + i);
+			
+			if(i != 10) {
+				thread.setPriority(Thread.MIN_PRIORITY);
+			}else {
+				thread.setPriority(Thread.MAX_PRIORITY);
+			}
+			thread.start();
+		}
+
+	}
+
+}
