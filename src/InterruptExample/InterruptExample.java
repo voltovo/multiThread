8bf00e8ff024e07a7d3946e31d1c7b5b0f14ab58
sumanChoi <voltovo87@gmail.com>
2020-02-17 14:02:57 +0900

    refactor
    스레드 예외발생 작성 완료

---
+++ src/InterruptExample/InterruptExample.java
@@ -0,0 +1,18 @@
+package InterruptExample;
+
+/**
+ * InterruptExample
+ */
+public class InterruptExample {
+    public static void main(String[] args) {
+        Thread thread = new PrintThread2();
+        thread.start();
+
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+        }
+        thread.interrupt();
+    }
+    
+}
---
+++ src/InterruptExample/PrintThread2.java
@@ -0,0 +1,18 @@
+package InterruptExample;
+
+/**
+ * PrintThread2
+ */
+public class PrintThread2 extends Thread{
+    public void run(){
+        try {
+            System.out.println("실행 중");
+            Thread.sleep(1);
+        } catch (InterruptedException e) {
+        }
+
+        System.out.println("자원 정리");
+        System.out.println("실행 종료");
+    }
+    
+}
